735f82625c6e146006d352c8a5edd657f8de6f98
DevKourav <Dhanshree270848@gmail.com>
2024-09-05 01:38:13 +0530

    cleanup

--- patient-login/plogin.js
+++ patient-login/plogin.js
@@ -1,7 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-
+import { getAuth, signInWithEmailAndPassword, 
+    onAuthStateChanged
+ } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+ import {
+    getFirestore, collection, onSnapshot,
+    addDoc, deleteDoc, doc,
+    query, where
+  } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js'
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBlLLPvziy_omkGwlaxsbtS8Kb29637KmM",
@@ -38,4 +44,20 @@ form.addEventListener('submit', async (event) => {
         loginError.textContent = `Error: ${error.message}`;
     }
 });
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    window.user_email = user.email;
+    console.log("user is signed in")
+    console.log(user_email)
+    // ...
+  } else {
+    // User is signed out
+    // ...
+    console.log("Pata nahi yhe kyu chl gaya")
+  }
+});
+
 
